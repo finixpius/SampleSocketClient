f290734ab488ad8170a5bece9c6ee06442281c30
finixpius <finixpius@gmail.com>
2020-05-04 22:40:51 -0400

    Intergrated Server and Client code to one project

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Permissions;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,73 +21,83 @@ namespace SocketProgramming
     {
         static string ip = "127.0.0.1";
         static Int32 port = 11000;
-        private const string incrementMsg = "increment";
-        private const string evaluateMsg = "evaluate";
-        static int counter = 0;
+        private const string INCREMENT_MSG = "increment";
+        private const string EVALUATE_MSG = "evaluate";
+        private const string CLOSE_MSG = "close";
+
         static void Main(string[] args)
         {
             // Start socket server and listening for the client
-            //Task.Run(() => StartScoketServer());
-
-            //Start client
-            Task.Run(() => StartClient1());
+            Task.Factory.StartNew(() => StartScoketServer());
 
             Console.ReadLine();
         }
 
-        private static void StartScoketServer()
+        /// <summary>
+        /// To start server
+        /// </summary>
+        public static void StartScoketServer()
         {
             // Establish the local endpoint for the socket.  
-            IPAddress ipAddress = IPAddress.Parse(ip);
-            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
 
-            Socket server = new Socket(ipAddress.AddressFamily,
+            // Create a TCP/IP socket.  
+            Socket server = new Socket(localEndPoint.AddressFamily,
                 SocketType.Stream, ProtocolType.Tcp);
 
-            // listen for incoming connections.  
             try
             {
+                byte[] bytes = new Byte[1024];
+                byte[] msg = new Byte[1024];
+
+                //Bind sever with end point.
                 server.Bind(localEndPoint);
+
                 server.Listen(10);
+
+                Console.WriteLine("Server: Started.");
+
+                //Start client
+                StartClient();
 
                 // Start listening for connections.  
                 while (true)
                 {
-                    Console.WriteLine("Waiting for a connection...");
+                    Socket handler = server.Accept();
 
-                    Socket client = server.Accept();
-                    string receivedData = null;
-                   
                     while (true)
                     {
-                        byte[] bytes = new Byte[1024];
-                        int bytesRec = client.Receive(bytes);
-                        receivedData = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                        string receiveData = string.Empty;
+                        int bytesRec = handler.Receive(bytes);
+                        receiveData = Encoding.ASCII.GetString(bytes, 0, bytesRec);
 
-                        switch (receivedData.ToLower())
+                        if (receiveData.Equals(CLOSE_MSG, StringComparison.OrdinalIgnoreCase))
                         {
-                            case incrementMsg:
-                                //TODO: increment value
-                                counter++;
-                                break;
-                            case evaluateMsg:
-                                //TODO: send back current value.
-                                byte[] msg = Encoding.ASCII.GetBytes(counter.ToString());
-                                client.Send(msg);
-                                break;
-                                //case "close":
-                                //    client.Shutdown(SocketShutdown.Both);
-                                //    client.Close();
-                                //    Console.WriteLine("XXXXXXXXXXXX Closed XXXXXXXXXXXXX");
-                                //    break;
+                            //Stop listening
+                            break;
                         }
-                       
-                        if (client.Available == 0) break;
+
+                        if (!string.IsNullOrEmpty(receiveData))
+                        {
+                            switch (receiveData.ToLower())
+                            {
+                                case INCREMENT_MSG:
+                                    Increment();
+                                    msg = Encoding.ASCII.GetBytes("success");
+                                    handler.Send(msg);
+                                    break;
+                                case EVALUATE_MSG:
+                                    int maxvalue = Evaluate();
+                                    receiveData = "Evaluate :" + maxvalue.ToString();
+                                    msg = Encoding.ASCII.GetBytes(receiveData);
+                                    handler.Send(msg);
+                                    break;
+                            }
+                        }
                     }
-                  
-                    client.Shutdown(SocketShutdown.Both);
-                    client.Close();
-                    
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
                 }
 
             }
@@ -92,207 +105,98 @@ namespace SocketProgramming
             {
                 Console.WriteLine(e.ToString());
             }
+
+            Console.WriteLine("\nServer: Press ENTER to continue...");
+            Console.Read();
+
         }
 
-        private static void StartClient()
+        /// <summary>
+        /// To start 500 clients
+        /// </summary>
+        public static void StartClient()
         {
-            for (int i = 0; i < 1; i++)
+            //Console.WriteLine("Client: Started");
+            byte[] bytesToRead = new byte[1024];
+            byte[] msgByte = new byte[1024];
+            int bytesRead;
+            string Message = string.Empty;
+
+            Task.Factory.StartNew(() =>
             {
-                byte[] bytes = new byte[1024];
-
-                // Connect to a server device.  
-                try
+                for (int i = 0; i < 500; i++)
                 {
-                    // Establish the remote endpoint for the socket.  
-
-                    IPAddress ipAddress = IPAddress.Parse(ip);
-                    IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
-
-                    // Create a TCP/IP  socket.  
-                    Socket client = new Socket(ipAddress.AddressFamily,
-                        SocketType.Stream, ProtocolType.Tcp);
-
-                    // Connect the socket to the server endpoint. 
-                    try
+                    Console.WriteLine("Client# " + i.ToString());
+                    using (var client = new TcpClient(ip, port))
                     {
-                        client.Connect(remoteEP);
-
-                        //Sleep for 5 seconds
                         Thread.Sleep(5000);
+                        var stream = client.GetStream();
+                        {
+                            //Send "increment" message
+                            Message = INCREMENT_MSG;
+                            msgByte = Encoding.ASCII.GetBytes(Message);
+                            stream.Write(msgByte, 0, msgByte.Length);
 
-                        // Sending "increment" msg to server.
-                        byte[] msg = Encoding.ASCII.GetBytes(incrementMsg);
+                            //Receive "success" message from server
+                            bytesToRead = new byte[client.ReceiveBufferSize];
+                            bytesRead = stream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                            Console.WriteLine("Client:" + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
 
-                        // Send the data through the socket.  
-                        Console.WriteLine("Client {0}: Sending increment msg", i.ToString());
-                        int bytesSent = client.Send(msg);
+                            //Send "evaluate" message to server.
+                            Message = EVALUATE_MSG;
+                            msgByte = Encoding.ASCII.GetBytes(Message);
+                            stream.Write(msgByte, 0, msgByte.Length);
 
-                        int bytesRec = client.Receive(bytes);
-                        Console.WriteLine("Client {0}: Received increment msg:-{1}", i.ToString(), Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                            //Receive "evaluate" value from server
+                            bytesToRead = new byte[client.ReceiveBufferSize];
+                            bytesRead = stream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+                            Console.WriteLine("Client: " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
 
+                            //Send "close" message to server to listen for next client.
+                            Message = CLOSE_MSG;
+                            byte[] msgByte1 = Encoding.ASCII.GetBytes(Message);
+                            stream.Write(msgByte1, 0, msgByte1.Length);
+                        }
 
-                        //Sending "evaluate" to server.
-                        msg = Encoding.ASCII.GetBytes(evaluateMsg);
-
-                        // Send the data through the socket.  
-                        Console.WriteLine("Client {0}: Sending evaluate msg", i.ToString());
-                        bytesSent = client.Send(msg);
-
-
-                        // Receive response from the server.  
-                        bytesRec = client.Receive(bytes);
-                        Console.WriteLine("Client {0}: Received evaluate msg:-{1}", i.ToString(), Encoding.ASCII.GetString(bytes, 0, bytesRec));
-
-                        bytesSent = client.Send(Encoding.ASCII.GetBytes("close"));
-                        // Release the client.  
-                        client.Shutdown(SocketShutdown.Both);
                         client.Close();
-
                     }
-                    catch (ArgumentNullException ane)
-                    {
-                        Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-                    }
-                    catch (SocketException se)
-                    {
-                        Console.WriteLine("SocketException : {0}", se.ToString());
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine("Unexpected exception : {0}", e.ToString());
-                    }
-
+                    Console.WriteLine("--------------------------------------\n");
                 }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.ToString());
-                }
-                Console.WriteLine("--------------------------------------------------------------\n");
-
-            }
+            });
         }
 
-
-        public static void StartClient1()
+        /// <summary>
+        /// Increment value by 1
+        /// </summary>
+        private static void Increment()
         {
-            Console.WriteLine("Started");
-            string message;
-
-            for (int i = 0; i < 20; i++)
+            int maxvalue = 0;
+            string strqry = "SELECT MAX(Value) FROM Count";
+            DataTable dtMax = DBConnectionManager.Instance.GetData(strqry);
+            if (dtMax != null && dtMax.Rows.Count > 0 && dtMax.Rows[0][0] != DBNull.Value)
             {
-                Console.WriteLine("Client "+i.ToString());
-                using (var client = new TcpClient(ip, port))
-                {
-                    if (!client.Connected) continue;
-                    var stream = client.GetStream();
-                    //var reader = new StreamReader(stream, Encoding.ASCII);
-                    //var writer = new StreamWriter(stream, Encoding.ASCII);
-
-                    bool stopRequested;
-
-                    //do
-                    {
-                        message = incrementMsg; //counter.ToString();
-                        byte[] msgByte = Encoding.ASCII.GetBytes(message);
-                        stream.Write(msgByte, 0, msgByte.Length);
-                        counter++;
-
-                        byte[] bytesToRead = new byte[client.ReceiveBufferSize];
-                        int bytesRead = stream.Read(bytesToRead, 0, client.ReceiveBufferSize);
-                        Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead, 0, bytesRead));
-
-
-                        message = evaluateMsg;//counter.ToString();
-                        msgByte = Encoding.ASCII.GetBytes(message);
-                        stream.Write(msgByte, 0, msgByte.Length);
-                        counter++;
-
-                        byte[] bytesToRead1 = new byte[client.ReceiveBufferSize];
-                        int bytesRead1 = stream.Read(bytesToRead1, 0, client.ReceiveBufferSize);
-                        Console.WriteLine("Received : " + Encoding.ASCII.GetString(bytesToRead1, 0, bytesRead1));
-
-                        message = "<EOF>"; //counter.ToString();
-                        byte[] msgByte1 = Encoding.ASCII.GetBytes(message);
-                        stream.Write(msgByte1, 0, msgByte1.Length);
-
-                        stopRequested = (counter > 10);
-                    } //while (!stopRequested);
-                   
-                    counter = 0;
-
-                    client.Close();
-                }
-                Console.WriteLine("--------------------------------------\n");
+                maxvalue = Convert.ToInt32(dtMax.Rows[0][0]) + 1;
             }
-            return;
 
-            // Data buffer for incoming data.  
-            byte[] bytes = new byte[1024];
+            strqry = string.Format("INSERT INTO Count(Value) VALUES ({0})", maxvalue);
+            DBConnectionManager.Instance.ExecuteQuery(strqry);
+        }
 
-            // Connect to a remote device.  
-            try
+        /// <summary>
+        /// Get latest value
+        /// </summary>
+        /// <returns></returns>
+        private static int Evaluate()
+        {
+            int maxvalue = 0;
+            string strqry = "SELECT MAX(Value) FROM Count";
+            DataTable dtMax = DBConnectionManager.Instance.GetData(strqry);
+            if (dtMax != null && dtMax.Rows.Count > 0)
             {
-                // Establish the remote endpoint for the socket.  
-                // This example uses port 11000 on the local computer.  
-              
-                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse(ip),port);
-
-                // Create a TCP/IP  socket.  
-                Socket sender = new Socket(remoteEP.AddressFamily,
-                    SocketType.Stream, ProtocolType.Tcp);
-
-                // Connect the socket to the remote endpoint. Catch any errors.  
-                try
-                {
-                    sender.Connect(remoteEP);
-
-                    Console.WriteLine("Socket connected to {0}",
-                        sender.RemoteEndPoint.ToString());
-
-                    // Encode the data string into a byte array.  
-                    byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
-
-                    // Send the data through the socket.  
-                    int bytesSent = sender.Send(msg);
-
-                    // Receive the response from the remote device.  
-                    int bytesRec = sender.Receive(bytes);
-                    Console.WriteLine("Echoed test = {0}",
-                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
-                    //-----------------------------------------------
-
-                    sender.Connect(remoteEP);
-
-                     bytesSent = sender.Send(msg);
-
-                    // Receive the response from the remote device.  
-                     bytesRec = sender.Receive(bytes);
-                    Console.WriteLine("Echoed test = {0}",
-                        Encoding.ASCII.GetString(bytes, 0, bytesRec));
-
-                    // Release the socket.  
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-
-                }
-                catch (ArgumentNullException ane)
-                {
-                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-                }
-                catch (SocketException se)
-                {
-                    Console.WriteLine("SocketException : {0}", se.ToString());
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
-                }
-
+                maxvalue = Convert.ToInt32(dtMax.Rows[0][0]);
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
+            return maxvalue;
         }
     }
+
 }
